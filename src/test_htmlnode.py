0870ea46dc77308f00d2ea13e7b25994066f96a8
SpencerMcFadden <mcfadden.spencer@protonmail.com>
2026-08-07 14:13:55 -0400

    adds leafnode child class to htmlnode and tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -46,6 +46,30 @@ class TestHTMLNode(unittest.TestCase):
         )
         self.assertEqual(
             "HTMLNode(p, This is an html node, children: None, {'href': 'https://test.com'})", 
+            repr(node)
+        )
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Hello, world!", { "href": "https://test.com", "target": "_blank" })
+
+        self.assertEqual(node.to_html(), '<a href="https://test.com" target="_blank">Hello, world!</a>')
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_leaf_repr(self):
+        node = LeafNode(
+            "p", 
+            "This is an leaf node", 
+            { "href": "https://test.com" }
+        )
+        self.assertEqual(
+            "LeafNode(p, This is an leaf node, {'href': 'https://test.com'})", 
             repr(node)
         )
 
