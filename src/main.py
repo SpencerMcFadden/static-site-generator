8d51fcd9154525b2382440b3b34eaffeb7ec503c
SpencerMcFadden <mcfadden.spencer@protonmail.com>
2026-08-11 23:26:04 -0400

    adds recursive static directory copy function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,28 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+path_static = "./static"
+path_public = "./public"
+
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    copy_contents_to_directory(path_static, path_public)
+
+
+def copy_contents_to_directory(source: str, destination: str) -> None:
+    if not os.path.exists(source):
+        raise ValueError("source does not exist")
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    os.mkdir(destination)
+
+    source_contents = os.listdir(source)
+    for content in source_contents:
+        working_path = os.path.join(source, content)
+        if os.path.isfile(working_path):
+            shutil.copy(working_path, destination)
+        else:
+            copy_contents_to_directory(working_path, os.path.join(destination, content))
 
 
 main()
